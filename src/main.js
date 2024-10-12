0f26e0a896384867647a944a1c37b091a9706cfe
iaalvearp <iaalvearp@gmail.com>
2024-10-12 14:08:34 -0500

    Adding compatibility WEBGL script

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -14,9 +15,25 @@ scene.add(cube);
 
 camera.position.z = 5;
 
+if (WebGL.isWebGL2Available()) {
+
+    // Initiate function or other initializations here
+    animate();
+
+} else {
+
+    const warning = WebGL.getWebGL2ErrorMessage();
+    document.getElementById('container').appendChild(warning);
+
+}
+
 function animate() {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
+
+document.addEventListener('resize', () => {
+    console.log(document.innerWidth)
+})
